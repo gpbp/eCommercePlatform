04c45150ab937650d19ed05116a1bd90e9c24232
gpbp <haitrung.pham6573@gmail.com>
2020-09-16 12:57:13 +0200

    recreate command's bean

--- src/com/trung/beans/Command.java
+++ src/com/trung/beans/Command.java
@@ -1,33 +1,69 @@
 package com.trung.beans;
 
 public class Command {
-	private String title;
+	private String date;
 	
-	private double value;
+	private Double value;
+	
+	private String paymentMethod;
+	
+	private String paymentStatus;
+	
+	private String shippingMethod;
+	
+	private String shippingStatus;
 	
 	private Client client;
 	
-	private String article;
-	
-	private int quantity;
-	
 	public Command() {
 	}
-
-	public String getTitle() {
-		return title;
+	
+	public String getDate() {
+		return date;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setDate(String date) {
+		this.date = date;
 	}
 
-	public double getValue() {
+	public Double getValue() {
 		return value;
 	}
 
-	public void setValue(double value) {
+	public void setValue(Double value) {
 		this.value = value;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getShippingMethod() {
+		return shippingMethod;
+	}
+
+	public void setShippingMethod(String shippingMethod) {
+		this.shippingMethod = shippingMethod;
+	}
+
+	public String getShippingStatus() {
+		return shippingStatus;
+	}
+
+	public void setShippingStatus(String shippingStatus) {
+		this.shippingStatus = shippingStatus;
 	}
 
 	public Client getClient() {
@@ -37,22 +73,5 @@ public class Command {
 	public void setClient(Client client) {
 		this.client = client;
 	}
-
-	public String getArticle() {
-		return article;
-	}
-
-	public void setArticle(String article) {
-		this.article = article;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
 
 }
